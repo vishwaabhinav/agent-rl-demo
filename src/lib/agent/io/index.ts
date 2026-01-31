ff8db36732b94cec7ea03b86760aafea32dc5e39
Abhinav Vishwa <68440262+vishwaabhinav@users.noreply.github.com>
2026-01-31 13:37:40 +0400

    feat(agent): add I/O adapter exports

---
+++ src/lib/agent/io/index.ts
@@ -0,0 +1,9 @@
+/**
+ * I/O Adapters
+ */
+
+export { TextIO } from "./text-io";
+export type { TextIOConfig } from "./text-io";
+
+export { VoiceIO } from "./voice-io";
+export type { VoiceIOConfig } from "./voice-io";
